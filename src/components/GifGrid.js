d271a637ad8103c4d7c09f6b6ebb7aa0cc4edf39
David Alan Dorantes Torres <david.dorantes.torres@gmail.com>
2021-11-15 12:06:09 -0600

    Carga inicial del proyecto v1.0.0

---
+++ src/components/GifGrid.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { GifGridItem } from './GifGridItem';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+
+export const GifGrid = ({ categoria }) => {
+
+
+    const { data: images, loading } = useFetchGifs(categoria);
+
+    return (
+        <>
+            <h3 className="animate__animated animate__fadeIn">{categoria}</h3>
+
+            {loading && <p className="animate__animated animate__flash">Loading</p>}
+            <div className="card-grid">
+                {
+                    images.map(img => (
+                        <GifGridItem
+                            key={img.id}
+                            {...img}
+                        />
+                    ))
+                }
+            </div>
+        </>
+    )
+}
+export default GifGrid;
